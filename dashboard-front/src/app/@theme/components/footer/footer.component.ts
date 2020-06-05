898e108bd84d71edca708910f9108d3fdc3b565a
unknown <noevasher@gmail.com>
2020-06-04 23:28:17 -0500

    start project

---
+++ dashboard-front/src/app/@theme/components/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-footer',
+  styleUrls: ['./footer.component.scss'],
+  template: `
+    <span class="created-by">
+      Created by <b><a href="https://xideral.co" target="_blank">Xideral</a></b> 2020
+    </span>
+    <div class="socials">
+      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
+      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
+    </div>
+  `,
+})
+export class FooterComponent {
+}
---
+++ dashboard-front/src/app/pages/pages-menu.ts
@@ -0,0 +1,52 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: 'DASHBOARDS',
+    group: true,
+  },
+  {
+    title: 'Charts',
+    icon: 'pie-chart-outline',
+    children: [
+      {
+        title: 'Echarts',
+        link: '/pages/charts/echarts',
+      },
+      {
+        title: 'Charts.js',
+        link: '/pages/charts/chartjs',
+      },
+      {
+        title: 'D3',
+        link: '/pages/charts/d3',
+      },
+    ],
+  },
+  {
+    title: 'FEATURES',
+    group: true,
+  },
+  {
+    title: 'Auth',
+    icon: 'lock-outline',
+    children: [
+      {
+        title: 'Login',
+        link: '/auth/login',
+      },
+      {
+        title: 'Register',
+        link: '/auth/register',
+      },
+      {
+        title: 'Request Password',
+        link: '/auth/request-password',
+      },
+      {
+        title: 'Reset Password',
+        link: '/auth/reset-password',
+      },
+    ],
+  },
+];
